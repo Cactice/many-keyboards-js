e01cd77007cb1decbcf7c40d108b6ef419cb59d8
Cactice <14835424+Cactice@users.noreply.github.com>
2020-05-05 21:33:52 +0900

    Accept impossible nature of this and move on

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,24 @@ function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
   return element;
 }
 
+async function getDeviceList(){
+  let device = await navigator.usb.requestDevice({
+    'filters': [
+      {'classCode': 3} // USB HID devices
+    ]
+  })
+  await device.open()
+  await device.selectConfiguration(1)
+  await device.claimInterface(0)
+  let data = await device.transferIn(1, 8)
+  console.log(data)
+}
+
 document.body.appendChild(component());
+const button = document.createElement('BUTTON')
+button.innerText = 'click me'
+button.onclick = getDeviceList
+document.body.appendChild(button)
